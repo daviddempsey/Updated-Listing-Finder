055a6628441b1a773fd07efd2343ab5ab27f5905
David Dempsey <davidgdempsey@gmail.com>
2018-11-10 21:53:12 -0800

    Program now does what it's supposed to do :)

--- finder.py
+++ finder.py
@@ -1,17 +1,11 @@
 from datascience import *
 import numpy as np
-#import matplotlib
-import matplotlib.pyplot as plt
-#import pandas as pd
-#import IPython
-#import scipy
-plt.style.use('fivethirtyeight')
 import requests
 from bs4 import BeautifulSoup
 import time
 
 
-def craigslist_titles(search):
+def craigslist_titles_array(search):
     url = 'https://sandiego.craigslist.org/search/sss?sort=date&query=' + search
     html = requests.get(url).text
     soup = BeautifulSoup(html, features="html.parser")
@@ -22,7 +16,7 @@ def craigslist_titles(search):
     return result_titles
 
 
-def ebay_titles(search):
+def ebay_titles_array(search):
     url = 'https://www.ebay.com/sch/i.html?_from=R40&_nkw=' + search + '&_sacat=0'
     html = requests.get(url).text
     soup = BeautifulSoup(html, features="html.parser")
@@ -32,20 +26,20 @@ def ebay_titles(search):
         if 'SPONSORED' in text.get_text():
             pass
         else:
-            result_titles = np.append(result_titles, text.get_text())
+            if 'New Listing' in text.get_text():
+                result_titles = np.append(result_titles, text.get_text()[11:])
+            else:
+                result_titles = np.append(result_titles, text.get_text())
     return result_titles
 
 
-# def new_titles(search):
-#     craigslist_updated_titles = craigslist_titles(search)
-#     ebay_updated_titles = ebay_titles(search)
-
 def print_listings(titles):
-    for each in titles:
-        print(each)
+    if titles is not None:
+        for each in titles:
+            print(each)
 
 
-def updated_check(titles, updated_titles):
+def updated(titles, updated_titles):
     if titles[0] != updated_titles[0]:
         return True
     else:
@@ -54,34 +48,39 @@ def updated_check(titles, updated_titles):
 
 def new_listing_output(titles, updated_titles):
     new_listings = make_array()
-    if updated_check(titles, updated_titles):
-        for new in updated_titles:
-            if new != titles[0]:
-                new_listings = np.append(new_listings, new)
-    return new_listings
+    i = 0
+    while updated_titles.item(i) != titles.item(0):
+        new_listings = np.append(new_listings, updated_titles.item(i))
+        i += 1
+        if updated_titles.item(i) == titles.item(0):
+            return new_listings
+
 
 exit = False
 
 search_query = input('Enter what you are looking for: ')
 
-#craigslist_initial_titles = craigslist_titles(search_query)
-ebay_initial_titles = ebay_titles(search_query)
+craigslist_titles = craigslist_titles_array(search_query)
+ebay_initial_titles = ebay_titles_array(search_query)
+
+print('\nCraigslist Listings: \n')
+print_listings(craigslist_titles)
+time.sleep(2)
+print('\n\nEbay Listings: \n')
+print_listings(ebay_initial_titles)
+
+print('\nNew listings will refresh every 10 minutes')
 
 while not exit:
-    #print_listings(craigslist_initial_titles)
-    print(ebay_initial_titles)
+    time.sleep(10*60)
 
-    time.sleep(3)
+    craigslist_updated_titles = craigslist_titles_array(search_query)
+    ebay_updated_titles = ebay_titles_array(search_query)
 
-    #craigslist_updated_titles = craigslist_titles(search_query)
-    ebay_updated_titles = ebay_titles(search_query)
-    ebay_updated_titles = np.concatenate((make_array('wassup'), ebay_updated_titles))
-    # print(ebay_updated_titles)
+    if updated(craigslist_titles, craigslist_updated_titles) or updated(ebay_initial_titles, ebay_updated_titles):
+        print('\n\nNew Listings: ')
+        print_listings(new_listing_output(craigslist_titles, craigslist_updated_titles))
+        print_listings(new_listing_output(ebay_initial_titles, ebay_updated_titles))
 
-    #print_listings(new_listing_output(craigslist_initial_titles, craigslist_updated_titles))
-    print(new_listing_output(ebay_initial_titles, ebay_updated_titles))
-
-    #craigslist_initial_titles = craigslist_updated_titles
-    ebay_initial_titles = ebay_updated_titles
-
-    exit = True
+        craigslist_titles = craigslist_updated_titles
+        ebay_initial_titles = ebay_updated_titles
